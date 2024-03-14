f729446b0437846424d5c3fcf69da0eb523c406a
SeongchanJ <008373034@coyote.csusb.edu>
2024-03-14 13:28:08 -0700

    Initial commit

---
+++ Lab/Lab01/Lab_1_template.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main() {
+    int num1, num2;
+    
+    std::cout << "첫 번째 숫자를 입력하세요: ";
+    std::cin >> num1;
+    
+    std::cout << "두 번째 숫자를 입력하세요: ";
+    std::cin >> num2;
+    
+    int sum = num1 + num2;
+    
+    std::cout << "두 숫자의 합: " << sum << std::endl;
+    
+    return 0;
+}
+
