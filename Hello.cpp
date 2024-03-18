0a70a869ca608cb292c3541ebd9a8b3eafabd45b
SeongchanJ <161398907+SeongchanJ@users.noreply.github.com>
2024-03-18 14:42:42 -0700

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,15 @@
+/*
+* Name: Seongchan Jung
+* Filename: Hello.cpp
+* Program: Simple C++ Program that has output and input
+* Date: 2024.02.27
+*/
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{ cout << "Hello World!" << endl;
+	printf("Hello World again!\n");
+	return 0;
+}
